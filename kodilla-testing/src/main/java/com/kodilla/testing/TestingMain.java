f22371916480c324ca275353b0a4c556c6d5edf2
BfDmichal <michal.przyborowski@o2.pl>
2019-04-10 23:22:39 +0200

    Third commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Wprowadzenie do testowania");
+        System.out.println("Test klasy Calculator:");
+        System.out.println("add method test:");
+        int resultAdd = Calculator.add(3,4);
+        int resultSubtract = Calculator.subtract(3,2);
+
+        if(resultAdd==7){
+            System.out.println("Calculator add method is ready to use");
+        }
+        else{
+            System.out.println("Error, problem with add method");
+        }
+        System.out.println("subtract method test:");
+        if (resultSubtract==1){
+            System.out.println("Calculator subtract method is ready to use");
+        }
+        else{
+            System.out.println("Error, problem with subtract method");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.user;
+
+public class SimpleUser {
+    private String name;
+    public SimpleUser(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+}
