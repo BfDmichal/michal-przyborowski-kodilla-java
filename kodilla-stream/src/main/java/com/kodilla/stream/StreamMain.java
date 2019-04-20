7bd1b2de38015b65d883b9ea57fdab4070b9fa4b
BfDmichal <michal.przyborowski@o2.pl>
2019-04-20 22:13:04 +0200

    Commit 7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,15 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Walcome in modulo 7");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Group",(string)-> string+"A");
+        poemBeautifier.beautify("oslo",(string)-> string.toUpperCase());
+        poemBeautifier.beautify("culture",(string)-> "CBA"+string);
+        poemBeautifier.beautify("  Convert simple  ",(a)-> a.trim());
     }
 }
